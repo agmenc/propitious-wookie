24897d9c1883e1d8cecb87be1bd35c10a8112057
agmenc <chris.agmen-smith@theorem.technology>
2015-10-29 16:21:20 +0000

    Comet example

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,23 @@
 package controllers
 
+import play.api.libs.iteratee.Enumerator
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.Comet
+
+import scala.concurrent.Future
 
 class Application extends Controller {
-  def index = Action { implicit request =>
-    Ok("Got request [" + request + "]")
+  def index = Action.async { implicit request =>
+    futureString(request).map {s => Ok(s)}
+  }
+
+  private def futureString(request: Request[_]): Future[String] = scala.concurrent.Future {
+    "Got async request [" + request + "]"
+  }
+
+  def comet = Action {
+    val events = Enumerator("one", "two", "three")
+    Ok.chunked(events &> Comet(callback = "console.log"))
   }
 }
